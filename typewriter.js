374281de90a664ecb6ea9d566867f8c1575e5fe2
priyaprecious <priyanka.datti@gmail.com>
2021-11-03 23:01:19 +0000

    animated line

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (const char of sentence) {
+    
+    setTimeout(() => {
+        // print the char here
+        process.stdout.write(char);
+      }, delay)
+      delay += 50; // <= 1s delay to make it noticeable. Can dial it down later.
+    }
+    
+  
